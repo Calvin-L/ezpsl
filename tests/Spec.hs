11cb063f8690630b12de20ff28daa641fce22e67
Calvin Loncaric <marvinx03@gmail.com>
2020-03-10 22:29:14 -0700

    Add a few more parser tests

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -12,6 +12,12 @@ main = hspec $ do
     it "allows multiple strings on one line" $ do
       fmap stripLocations (parseExpression "\"hello\" \\o \"goodbye\"") `shouldBe`
         Right (EBinaryOp () Concat (EStr () "hello") (EStr () "goodbye"))
+    it "has correct associativity for + and *" $ do
+      fmap stripLocations (parseExpression "1 + 2 * 3") `shouldBe`
+        Right (EBinaryOp () Plus (EInt () 1) (EBinaryOp () Times (EInt () 2) (EInt () 3)))
+    it "computes parenthesized expressions first" $ do
+      fmap stripLocations (parseExpression "(1 + 2) * 3") `shouldBe`
+        Right (EBinaryOp () Times (EBinaryOp () Plus (EInt () 1) (EInt () 2)) (EInt () 3))
 
 stripLocations :: Exp t -> Exp ()
 stripLocations = fmap (const ())
